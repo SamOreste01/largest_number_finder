ab15a4fdaf50a629d80f332370b37dfa1ed2b772
Sam Oreste <samoreste50@gmail.com>
2024-10-16 23:40:35 +0800

    Comparing num2 to other numbers

--- largest_number_finder.py
+++ largest_number_finder.py
@@ -9,6 +9,8 @@ num5 = int(input("Please Enter the Fifth Number: "))
 if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
     print(f"The Largest Number: {num1}")
 #Comparing num2 to other variables
+if num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
+    print(f"The Largest Number: {num2}")
 #Comparing num3 to other variables
 #Comparing num4 to other variables
 #Comparing num5 to other variables
