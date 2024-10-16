37fe463b57fb14fcfde4f739ae413048db1bbfed
Sam Oreste <samoreste50@gmail.com>
2024-10-16 23:57:21 +0800

    Fixed the issue where num5 is always printing

--- largest_number_finder.py
+++ largest_number_finder.py
@@ -1,22 +1,23 @@
-#Input 5 numbers
+# Input 5 numbers
 num1 = int(input("Please Enter the First Number: "))
 num2 = int(input("Please Enter the Second Number: "))
 num3 = int(input("Please Enter the Third Number: "))
 num4 = int(input("Please Enter the Fourth Number: "))
 num5 = int(input("Please Enter the Fifth Number: "))
 
-#Comparing num1 to other variables
+# Comparing num1 to other variables
 if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
     print(f"The Largest Number: {num1}")
-#Comparing num2 to other variables
-if num2 > num1 and num2 > num3 and num2 > num4 and num2 > num5:
+# Comparing num2 to other variables
+elif num2 > num1 and num2 > num3 and num2 > num4 and num5:
     print(f"The Largest Number: {num2}")
-#Comparing num3 to other variables
-if num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
+# Comparing num3 to other variables
+elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
     print(f"The Largest Number: {num3}")
-#Comparing num4 to other variables
-if num4 > num1 and num4> num2 and num4 > num3 and num4 > num5:
+# Comparing num4 to other variables
+elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
     print(f"The Largest Number: {num4}")
-#Comparing num5 to other variables
+# If none of the conditions are true, num5 is the largest
 else:
     print(f"The Largest Number: {num5}")
+
