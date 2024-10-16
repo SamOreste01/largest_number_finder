6c0f5381126a0363282f6fbd3ea7457f6f110a79
Sam Oreste <samoreste50@gmail.com>
2024-10-16 23:43:36 +0800

    Comparing num5 to other numbers

--- largest_number_finder.py
+++ largest_number_finder.py
@@ -18,3 +18,5 @@ if num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
 if num4 > num1 and num4> num2 and num4 > num3 and num4 > num5:
     print(f"The Largest Number: {num4}")
 #Comparing num5 to other variables
+else:
+    print(f"The Largest Number: {num5}")
