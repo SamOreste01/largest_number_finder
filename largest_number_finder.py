e6ad8fc0b73de966f07d8b72635c58d05b5e5ac6
Sam Oreste <samoreste50@gmail.com>
2024-10-16 23:36:21 +0800

    Creating the input variables

--- largest_number_finder.py
+++ largest_number_finder.py
@@ -1,4 +1,10 @@
 #Input 5 numbers
+num1 = int(input("Please Enter the First Number: "))
+num2 = int(input("Please Enter the Second Number: "))
+num3 = int(input("Please Enter the Third Number: "))
+num4 = int(input("Please Enter the Fourth Number: "))
+num5 = int(input("Please Enter the Fifth Number: "))
+
 #Comparing num1 to other variables
 #Comparing num2 to other variables
 #Comparing num3 to other variables
