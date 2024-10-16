4a8843dc1c28de45be3f5b673ee921a80d59c04e
Sam Oreste <samoreste50@gmail.com>
2024-10-16 23:27:38 +0800

    Pseudocode

---
+++ largest_number_finder.py
@@ -0,0 +1,6 @@
+#Input 5 numbers
+#Comparing num1 to other variables
+#Comparing num2 to other variables
+#Comparing num3 to other variables
+#Comparing num4 to other variables
+#Comparing num5 to other variables
