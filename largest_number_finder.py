dc93678e840774c1a43221aae553b7c6c9ce3d4a
Sam Oreste <samoreste50@gmail.com>
2024-10-17 15:13:45 +0800

    Changed the > to >=

--- largest_number_finder.py
+++ largest_number_finder.py
@@ -6,16 +6,16 @@ num4 = int(input("Please Enter the Fourth Number: "))
 num5 = int(input("Please Enter the Fifth Number: "))
 
 # Comparing num1 to other variables
-if num1 > num2 and num1 > num3 and num1 > num4 and num1 > num5:
+if num1 >= num2 and num1 >= num3 and num1 >= num4 and num1 >= num5:
     print(f"The Largest Number: {num1}")
 # Comparing num2 to other variables
-elif num2 > num1 and num2 > num3 and num2 > num4 and num5:
+elif num2 >= num1 and num2 >= num3 and num2 >= num4 and num2 >= num5:
     print(f"The Largest Number: {num2}")
 # Comparing num3 to other variables
-elif num3 > num1 and num3 > num2 and num3 > num4 and num3 > num5:
+elif num3 >= num1 and num3 >= num2 and num3 >= num4 and num3 >= num5:
     print(f"The Largest Number: {num3}")
 # Comparing num4 to other variables
-elif num4 > num1 and num4 > num2 and num4 > num3 and num4 > num5:
+elif num4 >= num1 and num4 >= num2 and num4 >= num3 and num4 >= num5:
     print(f"The Largest Number: {num4}")
 # If none of the conditions are true, num5 is the largest
 else:
